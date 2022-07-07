b63efc593616ff9446254632704e0c2450becb61
Chris  Fynes <chris.fynes@fxdigital.uk>
2022-07-07 10:14:57 +0100

    ensure util is returning value

--- src/API.tsx
+++ src/API.tsx
@@ -20,7 +20,7 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
     const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
     const data = await (await (await fetch(endpoint))).json();
-    console.log(data)
+    console.log("FETCH QUIZ QUESTIONS", data)
     return data.results.map((question: Question) => (
         {
             ...question,
